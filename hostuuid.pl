745e381c61932c5f97be6056b689930d9097aa05
Richard Hartmann <richih@net.in.tum.de>
2009-11-03 01:04:17 +0100

    hostuuid.pl supports creating UUIDs
    Still having a problem with opening files

--- hostuuid.pl
+++ hostuuid.pl
@@ -5,27 +5,33 @@
 use strict;
 use warnings;
 
-use File::Basename;
+use UUID::Tiny;
 use Getopt::Long;
+use File::Basename;
 
 my $version = '1.0';
 my $program = basename($0);
 
 my $file = '/etc/hostuuid';
-my $help = $version = 0;
+my $print_version;
+my $print_help;
+my $create_uuid;
 
 
 GetOptions (
-            'file|f=s' => \$file,
-            'help|h|?' => \$help,
-            'version|v' => \$version,
-#            '' => \$,
+            'file|f=s'  => \$file,
+            'help|h|?'  => \$print_help,
+            'version|v' => \$print_version,
+            'create|c'  => \$create_uuid,
 #           '' => \$,
 #            '' => \$,
            );
 
 sub print_version() {
-	print "$program : version $version\n";
+	print "$program: version $version\n";
+	print "\n";
+	print "This program is released under GPLv3\n";
+	print "Written by Richard Hartmann for Debian and Grml\n";
 	exit 0;
 }
 
@@ -34,25 +40,44 @@ sub print_help() {
 	exit 0;
 }
 
-sub print_hostuuid() {
-	open (F, "< $file") or die "$program\: Could not open file: $file\: $!\n";
+sub create_uuid() {
+	my $uuid = find_hostuuid();
+	if ($uuid) {
+		print STDERR "$program: Found UUID '$uuid' in '$file'. Aborting!\n";
+		exit 101;
+	} else {
+		open (F, ">", "$file") or die "$program: Could not open file: '$file'\: $!\n";
+		$uuid = create_UUID_as_string('UUID_V4');
+		print "$program\: Stored UUID '$uuid' in file '$file'\n";
+		print F "$uuid\n";
+		close (F);
+		exit 0;
+	}
+}
+
+sub find_hostuuid() {
+	open (F, "<", "$file") or die "$program: Could not open file: '$file'\: $!\n";
 	while (<F>) {
 		if (/([0-9a-z]{8}-([0-9a-z]{4}-){3}[0-9a-z]{12})/i) {
-			print "$1\n";
 			close (F);
-			exit 0;
+			return $1;
 		}
 	}
-	print STDERR "$program\: Could not find UUID in $file\n";
-	exit 100;
 }
 
 sub main() {
-	print_version() if $version;
-	print_help()    if $help;
+	chomp $file;
+	print_version() if $print_version;
+	print_help()    if $print_help;
+	create_uuid()   if $create_uuid;
 
-	print "file: $file\n";
-	print_hostuuid();
+	my $uuid = find_hostuuid();
+	if ($uuid) {
+		print "$uuid\n";
+	} else {
+		print STDERR "$program: Could not find UUID in '$file'\n";
+		exit 100;
+}	
 }
 
 main();
