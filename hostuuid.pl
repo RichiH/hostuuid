dfcc29c9f17131f0606e7e247a6f7886e6ef7bac
Richard Hartmann <richih@net.in.tum.de>
2009-11-03 01:32:24 +0100

    Fixed elusive bug
    I read $file before (potentially) writing to it. So if it did not exist...

--- hostuuid.pl
+++ hostuuid.pl
@@ -15,7 +15,7 @@ my $program = basename($0);
 my $file = '/etc/hostuuid';
 my $print_version;
 my $print_help;
-my $create_uuid;
+my $create_uuid = 0;
 
 
 GetOptions (
@@ -28,15 +28,24 @@ GetOptions (
            );
 
 sub print_version() {
-	print "$program: version $version\n";
-	print "\n";
-	print "This program is released under GPLv3\n";
-	print "Written by Richard Hartmann for Debian and Grml\n";
+	print <<VERSION;
+$program: version $version
+
+This program is released under GPLv3
+Written by Richard Hartmann for Debian and Grml
+VERSION
 	exit 0;
 }
 
 sub print_help() {
-	print "help placeholder\n";
+	print <<HELP;
+Help for $program:
+
+  -h, -?, --help : print this help
+  -v, --version  : print version
+  -f, --file     : file to read from/write to. Defaults to /etc/hostuuid
+  -c, --create   : create & save UUID
+HELP
 	exit 0;
 }
 
@@ -47,7 +56,7 @@ sub create_uuid() {
 		exit 101;
 	} else {
 		open (F, ">", "$file") or die "$program: Could not open file: '$file'\: $!\n";
-		$uuid = create_UUID_as_string('UUID_V4');
+		$uuid = create_UUID_as_string(UUID_V4);
 		print "$program\: Stored UUID '$uuid' in file '$file'\n";
 		print F "$uuid\n";
 		close (F);
@@ -56,6 +65,8 @@ sub create_uuid() {
 }
 
 sub find_hostuuid() {
+#	if ($create_uuid) {return 0 unless -e $file;}
+	return 0 unless ((-e $file && $create_uuid) || !$create_uuid);
 	open (F, "<", "$file") or die "$program: Could not open file: '$file'\: $!\n";
 	while (<F>) {
 		if (/([0-9a-z]{8}-([0-9a-z]{4}-){3}[0-9a-z]{12})/i) {
@@ -66,6 +77,7 @@ sub find_hostuuid() {
 }
 
 sub main() {
+	print "\n\nIf anyone knows how to die() when there is no value for -f, tell me!\n\n";
 	chomp $file;
 	print_version() if $print_version;
 	print_help()    if $print_help;
